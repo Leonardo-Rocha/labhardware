7a545a8eaf4b419fb879fd9dae952be6c032a503
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-11-05 15:09:14 -0400

    Add line to ignore output files Add alu implementation using the other entities

--- quiz1/alu.vhd
+++ quiz1/alu.vhd
@@ -13,11 +13,18 @@ entity alu is
 end entity;
 
 architecture structural of alu is
-
--- SEU CODIGO AQUI
+    signal S_andor, S_addsub: std_logic_vector(N-1 downto 0);
 
 begin
+    andor: entity work.andor(behavioral)
+        generic map(N => N)
+        port map(A => A, B => B, operation => C(1), S => S_andor);
+    
+    addsub: entity work.addsub(behavioral)
+        generic map(N => N)
+        port map(A => A, B => B, operation => C(2), S => S_addsub);
 
--- SEU CODIGO AQUI
-
+    multiplex2x1: entity work.multiplex2x1(behavioral)
+        generic map(N => N)
+        port map(input0 => S_andor, input1 => S_addsub, sel => C(0), output => S);
 end architecture;
